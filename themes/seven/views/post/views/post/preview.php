4b32df67e5664240009307f61708647b4e6fb9f5
Mohammad Moein Hosseini Manesh <moein7tl@gmail.com>
2015-03-18 12:25:29 +0330

    support aparat,vimeo and youtube video HtmlPurifier

--- themes/seven/views/post/views/post/preview.php
+++ themes/seven/views/post/views/post/preview.php
@@ -1,14 +1,19 @@
 <?php 
 use yii\helpers\HtmlPurifier;
 use app\components\Helper\Extract;
+use app\modules\user\models\User;
 $this->registerAssetBundle('show-post');
+$title      =   Extract::extractTitle($model->autosave_content);
+$content    =   Extract::extractContent($model->autosave_content);
 ?>
-<section id="start" class="herofade hero cover element-img" style="opacity: 1; height: 302px; background-image: url(http://pixabay.com/get/1d2b7cc3baef30612217/1426148267/ac2ee8a2cc7706534145d0c1_640.jpg);">
+<section id="start" class="herofade hero cover element-img" style="opacity: 1; height: 302px; background-image: url(http://pixabay.com/get/34d83a3ef7abe78d2524/1426629548/25026f409649eb72bba49266_640.jpg);">
     <div class="tagline">
-        <img src="<?= Yii::$app->user->getIdentity()->getProfilePicture();?>" class="img-circle wow fadeInUp animated" alt="Columbia Logo" data-wow-delay="0.3s" style="visibility: visible; -webkit-animation-delay: 0.3s;">
+        <a href="<?= Yii::$app->urlManager->createUrl(["@{$model->getUser()->one()->username}"]) ?>">
+            <img src="<?= Yii::$app->user->getIdentity()->getProfilePicture();?>" class="img-circle wow fadeInUp animated" alt="Columbia Logo" data-wow-delay="0.3s" style="visibility: visible; -webkit-animation-delay: 0.3s;">    
+        </a>
         <hr class="mini white central">
-        <h1 class="wow fadeInUp animated" data-wow-delay="0.5s" style="visibility: visible; -webkit-animation-delay: 0.5s;">
-            
+        <h1 class="wow fadeInUp animated post-header" data-wow-delay="0.5s" style="visibility: visible; -webkit-animation-delay: 0.5s;">
+            <?= HtmlPurifier::process($title); ?>
         </h1>
         <a class="scrollto" href="#content"><span class="icon-arrow-down2"></span></a>
     </div>
@@ -24,8 +29,13 @@ $this->registerAssetBundle('show-post');
                             <div id="editor">
                                 <section class="section--first section--last">
                                     <div class="section-content"> 
-                                        <div class="section-inner layoutSingleColumn"> 
-                                            <?= $model->autosave_content; ?>
+                                        <div class="section-inner layoutSingleColumn" style="padding-top: 35px;"> 
+                                            <?php
+                                                $config = HTMLPurifier_Config::createDefault();
+                                                $config->set('HTML.SafeIframe', true);
+                                                $config->set('URI.SafeIframeRegexp', '%^(https?:)?//(www\.aparat\.com/video/video/embed/videohash/|www\.youtube(?:-nocookie)?\.com/embed/|player\.vimeo\.com/video/)%');
+                                                echo HtmlPurifier::process($content, $config); 
+                                            ?>
                                         </div>
                                     </div>
                                 </section>
