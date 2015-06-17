349bff0af64e20774f2c99facd8c4f3e828898d6
Mohammad Moein Hosseini Manesh <moein7tl@gmail.com>
2015-06-18 00:56:47 +0430

    fix migration bug

--- modules/post/migrations/m150408_074337_add_first_publish_timestamp_to_post.php
+++ modules/post/migrations/m150408_074337_add_first_publish_timestamp_to_post.php
@@ -8,7 +8,7 @@ class m150408_074337_add_first_publish_timestamp_to_post extends Migration
     public function up()
     {
         $table      =   Yii::$app->getModule('post')->postTable;
-        $this->addColumn($table, 'published_at', 'TIMESTAMP NOT NULL DEFAULT `0000-00-00 00:00:00` AFTER `updated_at`');
+        $this->addColumn($table, 'published_at', 'TIMESTAMP NOT NULL DEFAULT "0000-00-00 00:00:00" AFTER `updated_at`');
     }
 
     public function down()
