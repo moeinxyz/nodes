2099e1e8831f78af242defe3bfc7ffc931c5b84d
Mohammad Moein Hosseini Manesh <moein7tl@gmail.com>
2015-07-07 02:53:14 +0430

    add post score update requestor daemon

--- modules/post/controllers/PostSuggestionForGuestDaemonController.php
+++ modules/post/controllers/PostSuggestionForGuestDaemonController.php
@@ -2,11 +2,10 @@
 namespace app\modules\post\controllers;
 
 use Yii;
-
-
 use app\modules\post\Module;
 use app\modules\post\models\Post;
-use yii\db\Query;
+use filsh\yii2\gearman\JobWorkload;
+
 class PostSuggestionForGuestDaemonController extends \yii\console\Controller{
     
     public function init() {
@@ -18,70 +17,37 @@ class PostSuggestionForGuestDaemonController extends \yii\console\Controller{
     {
         do
         {
-            $timestamp      =   GuestToRead::find()->max('created_at');
-            $nearestTime    =   strtotime($timestamp);
-            $diffTime       =   time()  -   $nearestTime;
-
+            $timestamp      =   Post::find()->where('status=:status',[':status'=>  Post::STATUS_PUBLISH])
+                                    ->min('score_update_requested_at');
+            
+            $farthestTime   =   strtotime($timestamp);
+            $diffTime       =   time()  -   $farthestTime;
+            
             if ($timestamp != NULL && $diffTime < Module::CHECK_INTERVAL){
                 sleep(Module::CHECK_INTERVAL - $diffTime + Module::ADDITIONAL_SLEEP_SECS);
             }
             
+            $posts  =   Post::find()->select('id')->where('status=:status AND score_update_requested_at=:timestamp',[
+                ':status'       =>  Post::STATUS_PUBLISH,
+                ':timestamp'    =>  $timestamp
+            ])->limit(50)->all();
             
-            
-            
-            
-            $users  =   User::find()->select('id')->where('status=:status AND last_post_suggestion=:timestamp',[
-                ':status'    =>  User::STATUS_ACTIVE,
-                ':timestamp' =>  $timestamp
-            ])->limit(10)->all();
-            
-            foreach ($users as $user)
+            foreach ($posts as $post)
             {
-                if ($this->addMessageToBroker($user->id)){
-                    $user->last_post_suggestion = new \yii\db\Expression('NOW()');
-                    $user->save(false);
+                if ($this->addMessageToBroker($post->id)){
+                    $post->score_update_requested_at    =   new \yii\db\Expression('NOW()');
+                    $post->save(false);
                 } //  else try in next while loop
             }
         } while(true);
     }
 
-    
-    private function getLastWeekTopPost()
-    {
 
-        $sql    =   'SELECT id, comments_count, published_at, pure_text'
-            .' ,( SELECT COUNT(*) '   //correlated subquery
-            . 'FROM '.Userread::tableName()
-            . ' WHERE '.  Post::tableName().'.id = '.Userread::tableName().'.post_id'
-            . ') AS user_read_count '
-            .' ,( SELECT COUNT(*) '   //correlated subquery
-            . 'FROM '.Guestread::tableName()
-            . ' WHERE '.  Post::tableName().'.id = '.Guestread::tableName().'.post_id'
-            . ') AS guest_read_count '                
-            . ' FROM '.Post::tableName()
-            .       ' WHERE '.Post::tableName().'.status = :status AND '
-            .       Post::tableName().'.user_id != :user_id AND '                
-            .       Post::tableName().'.published_at > DATE_SUB(now(), INTERVAL 25 DAY) AND '
-            .       Post::tableName().'.id NOT IN '                
-            .       '(SELECT DISTINCT post_id FROM '.Userread::tableName().' WHERE user_id = :user_id) '
-            .       'ORDER BY '.Post::tableName().'.published_at';        
-        $query =    Yii::$app->db->createCommand($sql)
-                ->bindValue(':status', Post::STATUS_PUBLISH)
-                ->bindValue(':user_id', $userId);
-        
-        return $query->queryAll();
-        
-        
-        $query  =   new Query();
-//        $query->select('id, ')
-    }
-
-
-    private function addMessageToBroker($userId)
+    private function addMessageToBroker($postId)
     {
         try {
-            Yii::$app->gearman->getDispatcher()->background('PostSuggestionForUser', new JobWorkload([
-                'params' => ['userId'    =>  $userId]
+            Yii::$app->gearman->getDispatcher()->background('PostSuggestionForGuest', new JobWorkload([
+                'params' => ['$postId'    =>  $postId]
             ]));
             return true;
         } catch (\Sinergi\Gearman\Exception $ex) {
