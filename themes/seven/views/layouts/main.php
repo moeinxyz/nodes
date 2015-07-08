310344af0d95a9db3da3ad8e3569aea85edd25f5
Mohammad Moein Hosseini Manesh <moein7tl@gmail.com>
2015-07-08 21:37:32 +0430

    add google analytics

--- themes/seven/views/layouts/main.php
+++ themes/seven/views/layouts/main.php
@@ -1,9 +1,5 @@
 <?php
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
-
 /* @var $this \yii\web\View */
 /* @var $content string */
 
@@ -44,21 +40,14 @@ $this->registerJs($js);
             
             <?= (Yii::$app->user->isGuest)?$this->render('headers/login'):NULL; ?>
         <?php $this->endBody() ?>
-<!--     Piwik 
-    <script type="text/javascript">
-      var _paq = _paq || [];
-      _paq.push(['trackPageView']);
-      _paq.push(['enableLinkTracking']);
-      (function() {
-        var u="//t.nodes.ir/";
-        _paq.push(['setTrackerUrl', u+'piwik.php']);
-        _paq.push(['setSiteId', 1]);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-      })();
-    </script>
-    <noscript><p><img src="//t.nodes.ir/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
-     End Piwik Code         -->
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-38759523-10', 'auto');
+          ga('send', 'pageview');
+        </script>
     </body>    
 </html>
 <?php $this->endPage() ?>
