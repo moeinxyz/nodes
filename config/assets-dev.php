eeb6ebfb674e30ae5164585be6bb5d66890185f0
Mohammad Moein Hosseini Manesh <moein7tl@gmail.com>
2015-07-08 16:38:24 +0430

    dummy

--- config/assets-dev.php
+++ config/assets-dev.php
@@ -39,17 +39,17 @@ return [
     ],    
     'main'  =>  [
         'class'         =>  'yii\web\AssetBundle',
-        'sourcePath'    =>  '@app/themes/seven/assets',
+        'sourcePath'    =>  '@app/themes/seven/assets/master/',
         'css'           =>  [
-                                'master/css/icomoon.css',//https://icomoon.io/
-                                'master/css/style.css',
-                                'master/css/custom.css',
-                                'master/css/AdminLTE.css',
-                                'master/css/skin-blue.css',
+                                'css/icomoon.css',//https://icomoon.io/
+                                'css/style.css',
+                                'css/custom.css',
+                                'css/AdminLTE.css',
+                                'css/skin-blue.css',
                             ],
         'js'            => [
-                                'master/js/main.js',
-                                'master/js/app.min.js',
+                                'js/main.js',
+                                'js/app.min.js',
                             ],        
         'depends'       =>  ['init','wow']        
     ],
@@ -103,56 +103,4 @@ return [
         'css'           =>  ['css/jasny-bootstrap.min.css'],
         'depends'       =>  ['main']
     ],
-    
-    
-//    'Main2' => [
-//        'class'         =>  'yii\web\AssetBundle',
-//        'sourcePath'    =>  '@app/themes/seven/assets',
-//        'css'           =>  [
-//                                'css/animate.css',
-//                                'css/icomoon.css',
-//                                'css/style.css',
-//                                'css/custom.css',
-//
-//                                'dist/css/AdminLTE.css',
-//                                'dist/css/skins/_all-skins.css',
-//                                'plugins/iCheck/flat/blue.css',
-//                                'http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css',
-//                                'plugins/morris/morris.css',
-//                                'https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
-//                                'plugins/jvectormap/jquery-jvectormap-1.2.2.css',
-//                                'http://ifont.ir/apicode/17'
-//                            ],
-//        'js'            => [
-//                                'js/bootstrap.min.js',
-//                                'js/wow.min.js',
-//                                'js/jquery.nav.js',
-//                                'js/jquery.nicescroll.min.js',
-//                                'js/jquery.easing.min.js',
-//                                'js/scripts.js',
-//
-//                                'http://code.jquery.com/ui/1.11.2/jquery-ui.min.js',
-//                                'http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js',
-//                                'plugins/morris/morris.min.js',
-//                                'plugins/sparkline/jquery.sparkline.min.js',
-//                                'plugins/jvectormap/jquery-jvectormap-1.2.2.min.js',
-//                                'plugins/jvectormap/jquery-jvectormap-world-mill-en.js',
-//                                'plugins/jqueryKnob/jquery.knob.js',
-//                                'plugins/daterangepicker/daterangepicker.js',
-//                                'plugins/datepicker/bootstrap-datepicker.js',
-//                                'plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js',
-//                                'plugins/iCheck/icheck.min.js',
-//                                'plugins/slimScroll/jquery.slimscroll.min.js',
-//                                'dist/js/app.min.js',
-//                                'dist/js/pages/dashboard.js',
-//                                'dist/js/demo.js'            
-//                            ],
-//        'depends'       =>  [
-//                                'yii\web\YiiAsset',
-//                                'yii\bootstrap\BootstrapAsset',
-//                                'yii\bootstrap\BootstrapPluginAsset',
-//                                'yii\web\JqueryAsset',
-//                                'yii\jui\JuiAsset'            
-//                            ]
-//    ],
 ];
