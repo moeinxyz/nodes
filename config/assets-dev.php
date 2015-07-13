543748e6418b0b86545c1b905354ea2ed8cc5c3c
Mohammad Moein Hosseini Manesh <moein7tl@gmail.com>
2015-07-13 18:06:09 +0430

    complete assets minifications

--- config/assets-dev.php
+++ config/assets-dev.php
@@ -5,33 +5,18 @@ return [
     ],
     'app\\assets\\InitAssets'  =>  [
         'class'         =>  'app\assets\InitAssets',        
-        'depends'       =>  [
-                        'yii\web\YiiAsset',
-                        'yii\bootstrap\BootstrapPluginAsset',
-                        'app\\assets\\FontAwesomeAssets'
-                    ]        
     ],
     'app\\assets\\WowAssets'=>  [
         'class'         =>  'app\assets\WowAssets',
     ],    
     'app\\assets\\MainAssets'  =>  [
         'class'         =>  'app\\assets\\MainAssets',        
-        'depends'       =>  [
-            'app\\assets\\InitAssets',
-            'app\\assets\\WowAssets'
-        ]        
     ],
     'app\\assets\\ICheckAssets'    =>  [
         'class'         =>  'app\assets\ICheckAssets',
-        'depends'       =>  [
-            'app\\assets\\MainAssets'
-        ]
     ],
     'app\\assets\\BootstrapSwitchAssets'  =>  [
         'class'         =>  'app\assets\BootstrapSwitchAssets',
-        'depends'       =>  [
-            'app\\assets\\MainAssets'
-        ]        
     ],
     'app\\assets\\UnderscoreAssets'=>  [
         'class'         =>  'app\assets\UnderscoreAssets',
@@ -40,32 +25,15 @@ return [
         'class'         =>  'app\assets\SanitizeAssets',
     ],
     'app\\assets\\DanteAssets'     =>  [
-        'class'         =>  'app\assets\DanteAssets',
-        'depends'       =>  [
-            'yii\web\JqueryAsset',
-            'app\\assets\\SanitizeAssets',
-            'app\\assets\\UnderscoreAssets',
-            'app\\assets\\MainAssets'
-        ]
-        
+        'class'         =>  'app\assets\DanteAssets',        
     ],
     'app\\assets\\CustomEditorAssets'     =>  [
         'class'         =>  'app\assets\CustomEditorAssets',
-        'depends'       =>  [
-            'app\\assets\\MainAssets',
-            'app\\assets\\DanteAssets'
-        ]
     ],
     'app\\assets\\ShowPostAssets'  =>  [
         'class'         => 'app\assets\ShowPostAssets',
-        'depends'       =>  [
-            'app\\assets\\MainAssets',
-        ]
     ],
     'app\\assets\\JasnyBootstrapAssets'   =>  [
         'class'         =>  'app\assets\JasnyBootstrapAssets',
-        'depends'       =>  [
-            'app\\assets\\MainAssets',
-        ]
     ],
 ];
--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', YII_ENV_DEV);
+defined('YII_ENV') or define('YII_ENV', 'prod');
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
