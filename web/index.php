fd58bea49f76de8c537888bedd7d0b0be7c62ddd
Mohammad Moein Hosseini Manesh <moein7tl@gmail.com>
2015-07-14 19:39:51 +0430

    dummy

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_DEBUG') or define('YII_DEBUG', false);
 defined('YII_ENV') or define('YII_ENV', 'prod');
 
 require(__DIR__ . '/../vendor/autoload.php');
