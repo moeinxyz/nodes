d8fac82805badeb47b7471a0b00ff0e0e867917b
Mohammad Moein Hosseini Manesh <moein7tl@gmail.com>
2015-07-15 06:34:18 +0430

    fix dummy bugs

--- assets/BootstrapSwitchAssets.php
+++ assets/BootstrapSwitchAssets.php
@@ -6,11 +6,13 @@ use yii\web\AssetBundle;
 
 class BootstrapSwitchAssets extends AssetBundle
 {
-    public $sourcePath =   '@bower/components-font-awesome';
+    public $sourcePath =   '@bower/bootstrap-switch/dist';
     public $css = [
-        'css/font-awesome.min.css',
+        'css/bootstrap3/bootstrap-switch.min.css',
     ];
-    public $js = [];
+    public $js = [
+        'js/bootstrap-switch.min.js',
+    ];
     public $depends = [
         'app\assets\MainAssets'
     ];
