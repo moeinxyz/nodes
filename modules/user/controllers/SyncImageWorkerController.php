b598e729641383d0216136654dce2f02639fcfab
Mohammad Moein Hosseini Manesh <moein7tl@gmail.com>
2015-07-20 21:51:58 +0430

    add user module worker

---
+++ modules/user/controllers/SyncImageWorkerController.php
@@ -0,0 +1,98 @@
+<?php
+namespace app\modules\user\controllers;
+
+use Yii;
+use app\components\Broker;
+use yii\imagine\Image;
+use Imagine\Image\Box;
+use Imagine\Image\Point;
+
+class SyncImageWorkerController extends \yii\console\Controller{
+    
+    const TYPE_COVER    =   'COVER';
+    const TYPE_PROFILE  =   'PROFILE';
+    
+    public function init() {
+        parent::init();
+        set_time_limit(0);
+        register_shutdown_function(function (){
+            Broker::close();
+        });
+    }
+    
+    public function actionIndex(){
+        Broker::consumeMessage('SyncImage', function($message){
+            $params =   Json::decode($message->body);
+            $this->uploadImage($params['userId'], $params['image'],$params['type']);
+            Broker::sendAck($message);
+        });
+    }    
+
+    private function uploadImage($userId,$url,$type = self::TYPE_PROFILE)
+    {    
+        $stream     =   imagecreatefromstring(file_get_contents($url));
+        $fileName   =   md5($userId).base_convert($userId, 10, 36);
+        if ($type === self::TYPE_PROFILE){
+            $localFile  =   Yii::getAlias("@pictures/{$fileName}.jpg");
+            $remoteFile =   Yii::getAlias("@ftpPictures/{$fileName}.jpg");
+        } else {
+            $localFile  =   Yii::getAlias("@covers/{$fileName}.jpg");
+            $remoteFile =   Yii::getAlias("@ftpCovers/{$fileName}.jpg");
+        }
+        imagejpeg($stream,$localFile,100);
+        imagedestroy($stream);
+        $this->setStandardImageSize($localFile,$type);
+        $content = file_get_contents($localFile);
+        Yii::$app->ftpFs->put($remoteFile, $content);
+        unlink($localFile);        
+    }    
+    
+    private function setStandardImageSize($image,$type)
+    {
+        if ($type === self::TYPE_PROFILE){
+            $this->setStandardProfileImageSize($image);
+        } else {
+            $this->setStandardCoverImageSize($image);
+        }
+    }
+    
+    private function setStandardProfileImageSize($file)
+    {
+        $image          =   Image::getImagine()->open($file);
+        $width          =   $image->getSize()->getWidth();
+        $height         =   $image->getSize()->getHeight();
+        if ($width > $height) {
+            $start = new Point(($width - $height) / 2,0);
+            $size  = new Box($height,$height);
+        } else {
+            $start = new Point(0,($height - $width) / 2);
+            $size  = new Box($width,$width);
+        }
+        
+        $image->crop($start, $size)
+                ->resize(new Box(200, 200))
+                ->save($file);
+    }
+    
+    private function setStandardCoverImageSize($file)
+    {
+        $image          =   Image::getImagine()->open($file);
+        $width          =   $image->getSize()->getWidth();
+        $height         =   $image->getSize()->getHeight();
+        
+        if (($width / 1400) > ($height / 600)) {
+            $newWidth   =   ($height / 600) * 1400;
+            $image      =   $image->crop(new Point(($width - $newWidth) / 2,0), new Box($newWidth, $height));
+        } else {
+            $newHeight  =   ($width / 1400) * 600;
+            $image      =   $image->crop(new Point(0,($height - $newHeight) / 2), new Box($width, $newHeight));
+        }
+        
+        if ($image->getSize()->getWidth() > 1400)
+        {
+            $image = $image->resize(new Box(1400, 600));
+        }
+        
+        $image->save($file,['quality'=>80]);
+    }    
+}
