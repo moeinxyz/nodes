94b62e1c435c5e581ca025f4d18afd42cc32f9cd
Mohammad Moein Hosseini Manesh <moein7tl@gmail.com>
2015-08-06 00:21:15 +0430

    change signup suggestion for better mobile freindly

--- themes/seven/views/post/views/post/home/_signup_suggestion.php
+++ themes/seven/views/post/views/post/home/_signup_suggestion.php
@@ -7,7 +7,7 @@
             </div>
             <div class="box-body">
                 <?= Module::t('post', 'home._signup_suggestion.text'); ?>
-                <div class="row">
+                <div class="row hidden-xs">
                     <div class="col-sm-6 col-xs-12">
                         <a href="<?= Yii::$app->urlManager->createUrl(['user/join']); ?>" class="btn btn-flat btn-primary btn-block">
                             <?= Module::t('post', 'home._signup_suggestion.btn.register'); ?>
@@ -18,6 +18,18 @@
                             <?= Module::t('post', 'home._signup_suggestion.btn.login'); ?>
                         </a>
                     </div>
+                </div>
+                <div class="row hidden-lg hidden-md hidden-sm">
+                    <div class="col-xs-12">
+                        <a href="#" class="btn btn-flat btn-primary btn-block" id="home-login-button">
+                            <?= Module::t('post', 'home._signup_suggestion.btn.login'); ?>
+                        </a>
+                    </div>                    
+                    <div class="col-xs-12 top-buffer-light">
+                        <a href="<?= Yii::$app->urlManager->createUrl(['user/join']); ?>" class="btn btn-flat btn-primary btn-block">
+                            <?= Module::t('post', 'home._signup_suggestion.btn.register'); ?>
+                        </a>
+                    </div>                    
                 </div>
             </div>
         </div>        
