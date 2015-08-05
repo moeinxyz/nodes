48b5b909b887ed5ac7ff96fcc5ebaa955c9b8e89
Mohammad Moein Hosseini Manesh <moein7tl@gmail.com>
2015-08-05 16:02:13 +0430

    insert old pure_text to

--- modules/post/migrations/m150805_050158_add_pure_text_to_comments.php
+++ modules/post/migrations/m150805_050158_add_pure_text_to_comments.php
@@ -2,13 +2,18 @@
 
 use yii\db\Schema;
 use yii\db\Migration;
-
+use app\modules\post\models\Comment;
 class m150805_050158_add_pure_text_to_comments extends Migration
 {
     public function up()
     {
         $table      =   Yii::$app->getModule('post')->commentTable;
         $this->addColumn($table, 'pure_text', 'TEXT NOT NULL AFTER `text`');
+        foreach (Comment::findAll([]) as $comment)
+        {
+            $comment->pure_text = strip_tags($comment->text);
+            $comment->save();
+        }
     }
 
     public function down()
