7cb8934a2ccc4e99f35c489499a3bbfd257dbfc3
Mohammad Moein Hosseini Manesh <moein7tl@gmail.com>
2015-08-09 08:46:55 +0430

    rename some setting attrs

--- modules/user/messages/fa-IR/user.php
+++ modules/user/messages/fa-IR/user.php
@@ -106,11 +106,11 @@ return [
     'chgPwg.vld.wrongePassword'         =>  'کلمه عبور اشتباه می باشد.',
     
     // setting
-    'chgSetting.attr.content_activity'          =>  'فعالیت بر روی محتوی',
+    'chgSetting.attr.content_activity'          =>  'نظرات برای نوشته های شما',
     'chgSetting.attr.social_activity'           =>  'فعالیت های اجتماعی',
     'chgSetting.attr.reading_list'              =>  'نوشته های پیشنهاد شده',
     
-    'setting._setting.content_activity.hint'    =>  'نحوه مطلع نمودن شما از طریق ایمیل از نظرات بر روی نوشته های شما،پاسخ به نظرات شما و یا اشاره کردن به شما در نوشته ها و نظرات',
+    'setting._setting.content_activity.hint'    =>  'نحوه مطلع نمودن شما از طریق ایمیل از نظرات ارسال شده روی نوشته ها شما',
     'setting._setting.social_activity.hint'     =>  'نحوه مطلع نمودن شما از دنبال کنندگان جدید و یا پیوستن دوستان ',
     'setting._setting.reading_list.hint'        =>  'دریافت  نوشته های پیشنهاد شده و برتر',
     'setting.changeSettingBtn'                  =>  'تغییر تنظیمات',
