016958cf3614993303a75fa933533b15d3a58719
Moein Hosseini <moein7tl@gmail.com>
2016-07-01 15:10:22 +0430

    using piwik in production mode

--- themes/seven/views/layouts/main.php
+++ themes/seven/views/layouts/main.php
@@ -43,14 +43,22 @@ $this->registerLinkTag(['rel'=>'shortcut icon','href'=>Yii::$app->urlManager->ba
 
             <?= (Yii::$app->user->isGuest)?$this->render('headers/login'):NULL; ?>
         <?php $this->endBody() ?>
-        <script>
-//          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-//          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-//          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-//          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-//          ga('create', 'UA-38759523-10', 'auto');
-//          ga('send', 'pageview');
-        </script>
+        <?php if (YII_ENV_PROD):?>
+            <script type="text/javascript">
+                var _paq = _paq || [];
+                _paq.push(["setDomains", ["*.nodes.ir","*.nodes.ir"]]);
+                _paq.push(['trackPageView']);
+                _paq.push(['enableLinkTracking']);
+                (function() {
+                    var u="//stats.nodes.ir/";
+                    _paq.push(['setTrackerUrl', u+'piwik.php']);
+                    _paq.push(['setSiteId', 1]);
+                    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+                })();
+            </script>
+            <noscript><p><img src="//stats.nodes.ir/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+        <?php endif; ?>
     </body>    
 </html>
 <?php $this->endPage() ?>
