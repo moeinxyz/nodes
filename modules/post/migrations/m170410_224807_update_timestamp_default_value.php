71cb62c91be47f23c6b693a44ec78a74620a2470
Moein Hosseini <moein7tl@gmail.com>
2017-04-13 00:57:30 +0430

    * Change git repository for deployment * make all mysql keyword uniform in migration

--- modules/post/migrations/m170410_224807_update_timestamp_default_value.php
+++ modules/post/migrations/m170410_224807_update_timestamp_default_value.php
@@ -7,7 +7,7 @@ class m170410_224807_update_timestamp_default_value extends Migration
     public function up()
     {
         $table      =   Yii::$app->getModule('post')->postTable;
-        $this->alterColumn($table, 'updated_at', 'timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP');
+        $this->alterColumn($table, 'updated_at', 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP');
         $this->alterColumn($table, 'published_at', "TIMESTAMP NOT NULL DEFAULT '".Yii::$app->params['zeroTime']."'");
         $this->alterColumn($table, 'score_updated_at', "TIMESTAMP NOT NULL DEFAULT '".Yii::$app->params['zeroTime']."'");
         $this->alterColumn($table, 'score_update_requested_at', "TIMESTAMP NOT NULL DEFAULT '".Yii::$app->params['zeroTime']."'");
