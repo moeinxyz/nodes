f2586aaab65dd38671bcadcc181a101f1c668b68
Moein Hosseini <moein7tl@gmail.com>
2017-04-17 15:08:14 +0430

    * Add github repository link in footer

--- themes/seven/views/layouts/footer.php
+++ themes/seven/views/layouts/footer.php
@@ -3,10 +3,14 @@
         <div class="row"> 
             <div class="col-xs-12">
                 <small><?= Yii::t('app','footer'); ?></small>
-<!--                <a href="index.html" title="Columbia"><img src="img/f-logo.png" alt="Add alt tag"></a>
-                <p>Believe in what you know</p>
-                <small>&copy; Copyright 2014 Themes Collective. All rights reserved.</small>
-                <hr class="mini central light-grey-dk">-->
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <a class="github-button" href="https://github.com/moein7tl/nodes" data-count-href="/moein7tl/nodes/stargazers" data-count-api="/repos/moein7tl/nodes#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star moein7tl/nodes on GitHub">Star</a>
+                <a class="github-button" href="https://github.com/moein7tl/nodes/issues" data-count-api="/repos/moein7tl/nodes#open_issues_count" data-count-aria-label="# issues on GitHub" aria-label="Issue moein7tl/nodes on GitHub">Issue</a>
+                <a class="github-button" href="https://github.com/moein7tl/nodes/fork" data-count-href="/moein7tl/nodes/network" data-count-api="/repos/moein7tl/nodes#forks_count" data-count-aria-label="# forks on GitHub" aria-label="Fork moein7tl/nodes on GitHub">Fork</a>
+                <script async defer src="https://buttons.github.io/buttons.js"></script>
             </div>
         </div>
     </div>
