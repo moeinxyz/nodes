f41bb049c36dc5dbbf84c7a99a23913074ab7e10
Moein Hosseini <moein7tl@gmail.com>
2017-04-19 01:58:37 +0430

    * solve daylight problem

--- modules/post/helper/Helper.php
+++ modules/post/helper/Helper.php
@@ -9,13 +9,16 @@
 namespace app\modules\post\helper;
 
 use app\modules\post\models\Post;
+use Yii;
 
 class Helper {
     public static function getPostPublishTime(Post $post){
         $t1 =   strtotime($post->published_at);
-        $t2 =   strtotime(\Yii::$app->params['zeroTime']);
+        $t2 =   strtotime(Yii::$app->params['zeroTime']);
+        $diff = abs($t1 - $t2);
 
-        if ($t1 == $t2){
+        // To solve daylight in every server configuration!
+        if ($diff <= 3600 * 24){
             return time();
         }
 
